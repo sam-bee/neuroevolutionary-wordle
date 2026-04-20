2ef821c795a43d02074cc6814a31ad846476cb62
sam-bee <130986804+sam-bee@users.noreply.github.com>
2026-04-21 00:23:09 +0100

    To support tensor action scoring, add reusable fixed-word feature helpers

--- src/model/output_embedding/output_embedding.hpp
+++ src/model/output_embedding/output_embedding.hpp
@@ -24,6 +24,7 @@ static_assert(kOutputEmbeddingDimension > kWordFeatureDimension,
               "Output embedding dimension must leave room for trainable features.");
 
 using ActionEmbeddingVector = common::FixedBuffer<float, kOutputEmbeddingDimension>;
+using FixedWordFeatureVector = common::FixedBuffer<float, kWordFeatureDimension>;
 using TrainableActionEmbeddingTail = common::FixedBuffer<ParameterScalar, kTrainableFeatureDimension>;
 
 struct ActionEmbedding {
@@ -61,13 +62,23 @@ inline NEUROEVOLUTION_HOST_DEVICE float FixedWordFeatureValue(const std::uint8_t
 
 } // namespace detail
 
-inline NEUROEVOLUTION_HOST_DEVICE void MaterializeActionEmbedding(const ActionEmbedding &action_embedding,
-                                                                  ActionEmbeddingVector &embedding_vector) noexcept {
+inline NEUROEVOLUTION_HOST_DEVICE void MaterializeFixedWordFeatures(const wordle::Word &word,
+                                                                    FixedWordFeatureVector &fixed_features) noexcept {
     common::FixedBuffer<std::uint8_t, wordle::kAlphabetSize> letter_counts{};
-    detail::CountWordLetters(action_embedding.word, letter_counts);
+    detail::CountWordLetters(word, letter_counts);
 
     for (std::size_t letter_index = 0; letter_index < kWordFeatureDimension; ++letter_index) {
-        embedding_vector[letter_index] = detail::FixedWordFeatureValue(letter_counts[letter_index]);
+        fixed_features[letter_index] = detail::FixedWordFeatureValue(letter_counts[letter_index]);
+    }
+}
+
+inline NEUROEVOLUTION_HOST_DEVICE void MaterializeActionEmbedding(const ActionEmbedding &action_embedding,
+                                                                  ActionEmbeddingVector &embedding_vector) noexcept {
+    FixedWordFeatureVector fixed_features{};
+    MaterializeFixedWordFeatures(action_embedding.word, fixed_features);
+
+    for (std::size_t letter_index = 0; letter_index < kWordFeatureDimension; ++letter_index) {
+        embedding_vector[letter_index] = fixed_features[letter_index];
     }
 
     for (std::size_t trainable_index = 0; trainable_index < kTrainableFeatureDimension; ++trainable_index) {
@@ -84,22 +95,26 @@ MaterializeActionEmbedding(const ActionEmbedding &action_embedding) noexcept {
 }
 
 inline NEUROEVOLUTION_HOST_DEVICE float ScoreActionEmbedding(const PolicyVector &policy_vector,
-                                                             const ActionEmbedding &action_embedding) noexcept {
-    common::FixedBuffer<std::uint8_t, wordle::kAlphabetSize> letter_counts{};
-    detail::CountWordLetters(action_embedding.word, letter_counts);
-
+                                                             const FixedWordFeatureVector &fixed_word_features,
+                                                             const TrainableActionEmbeddingTail &trainable_tail) noexcept {
     float score = 0.0f;
 
     for (std::size_t letter_index = 0; letter_index < kWordFeatureDimension; ++letter_index) {
-        score += policy_vector[letter_index] * detail::FixedWordFeatureValue(letter_counts[letter_index]);
+        score += policy_vector[letter_index] * fixed_word_features[letter_index];
     }
 
     for (std::size_t trainable_index = 0; trainable_index < kTrainableFeatureDimension; ++trainable_index) {
-        score += policy_vector[kWordFeatureDimension + trainable_index] *
-                 common::ToFloat(action_embedding.trainable_tail[trainable_index]);
+        score += policy_vector[kWordFeatureDimension + trainable_index] * common::ToFloat(trainable_tail[trainable_index]);
     }
 
     return score;
+}
+
+inline NEUROEVOLUTION_HOST_DEVICE float ScoreActionEmbedding(const PolicyVector &policy_vector,
+                                                             const ActionEmbedding &action_embedding) noexcept {
+    FixedWordFeatureVector fixed_word_features{};
+    MaterializeFixedWordFeatures(action_embedding.word, fixed_word_features);
+    return ScoreActionEmbedding(policy_vector, fixed_word_features, action_embedding.trainable_tail);
 }
 
 inline NEUROEVOLUTION_HOST_DEVICE bool TrySelectBestAction(const PolicyVector &policy_vector,
